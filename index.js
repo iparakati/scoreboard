fce1b45f7672537b2f97f7ee5183e2847bd3da0a
iparakati <iparakati@gmail.com>
2024-08-12 19:12:17 -0500

    Uploaded initial files

---
+++ index.js
@@ -0,0 +1,35 @@
+let homeScore = document.getElementById("home-score")
+let guestScore = document.getElementById("guest-score")
+
+let homeScoreActual = 0
+let guestScoreActual = 0
+
+function addOneHome() {
+    homeScoreActual += 1
+    homeScore.textContent = homeScoreActual
+}
+
+function addTwoHome() {
+    homeScoreActual += 2
+    homeScore.textContent = homeScoreActual
+}
+
+function addThreeHome() {
+    homeScoreActual += 3
+    homeScore.textContent = homeScoreActual
+}
+
+function addOneGuest() {
+    guestScoreActual += 1
+    guestScore.textContent = guestScoreActual
+}
+
+function addTwoGuest() {
+    guestScoreActual += 2
+    guestScore.textContent = guestScoreActual
+}
+
+function addThreeGuest() {
+    guestScoreActual += 3
+    guestScore.textContent = guestScoreActual
+}
